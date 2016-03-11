517ca4b928009e7c0b85b661546953011efb7e93
Veliohart@gmail.com <velen.web>
2016-03-11 16:06:48 +0200

    test

--- WorkTalk/WorkTalk.WebApp/js/serv.js
+++ WorkTalk/WorkTalk.WebApp/js/serv.js
@@ -1,7 +1,7 @@
 var serv = angular.module('wsServ', []);
 
 serv.factory('wsServ', ['$rootScope', function ($rootScope) {
-  var uri = 'ws://' + window.location.hostname + window.location.pathname.replace('default.html', 'api/Chat');
+  var uri = 'ws://' + window.location.hostname + window.location.pathname.replace('default.html', 'api/Chat') + '?username=testUser';
   var socket = new WebSocket("ws://localhost");
 
   socket.onopen = function () {
@@ -12,7 +12,7 @@ serv.factory('wsServ', ['$rootScope', function ($rootScope) {
 
   socket.onmessage = function (event, $rootScope) {
     var inMessage = event.data;
-    $rootScope.chatStack.apply(inMessage);
+    $rootScope.chatStack = inMessage;
     return inMessage;
   };
 
